1cb381bafad53e0f295b479367d6701384a377f2
bonfy <foreverbonfy@163.com>
2017-11-27 15:08:06 +0800

    add echo code

---
+++ golang/echo/lesson-6/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Cat struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Dog struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Hamster struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+func yallo(c echo.Context) error {
+	return c.String(http.StatusOK, "yallo from the web side!")
+}
+
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\nand his type is: %s\n", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to lets us know if you want json or string data",
+	})
+}
+
+func addCat(c echo.Context) error {
+	cat := Cat{}
+
+	defer c.Request().Body.Close()
+
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("Failed reading the request body for addCats: %s\n", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &cat)
+	if err != nil {
+		log.Printf("Failed unmarshaling in addCats: %s\n", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("this is your cat: %#v\n", cat)
+	return c.String(http.StatusOK, "we got your cat!")
+}
+
+func addDog(c echo.Context) error {
+	dog := Dog{}
+
+	defer c.Request().Body.Close()
+
+	err := json.NewDecoder(c.Request().Body).Decode(&dog)
+	if err != nil {
+		log.Printf("Failed processing addDog request: %s\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is your dog: %#v", dog)
+	return c.String(http.StatusOK, "we got your dog!")
+}
+
+func addHamster(c echo.Context) error {
+	hamster := Hamster{}
+
+	err := c.Bind(&hamster)
+	if err != nil {
+		log.Printf("Failed processing addHamster request: %s\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	log.Printf("this is your hamster: %#v", hamster)
+	return c.String(http.StatusOK, "we got your hamster!")
+}
+
+func mainAdmin(c echo.Context) error {
+	return c.String(http.StatusOK, "horay you are on the secret amdin main page!")
+}
+
+/////////////////// Custom Middleware /////////////////////
+
+// ServerHeader : Set Server Header
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
+		return next(c)
+	}
+}
+
+func main() {
+	fmt.Println("Welcome to the server")
+
+	e := echo.New()
+
+	// user header middleware whole website
+	e.Use(ServerHeader)
+
+	g := e.Group("/admin")
+
+	// this logs the server interaction
+	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}]  ${status}  ${method} ${host}${path} ${latency_human}` + "\n",
+	}))
+
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		// check in the DB
+		if username == "jack" && password == "1234" {
+			return true, nil
+		}
+
+		return false, nil
+	}))
+
+	g.GET("/main", mainAdmin)
+
+	e.GET("/", yallo)
+	e.GET("/cats/:data", getCats)
+
+	e.POST("/cats", addCat)
+	e.POST("/dogs", addDog)
+	e.POST("/hamsters", addHamster)
+
+	e.Start(":8888")
+}
---
+++ golang/echo/readme.md
@@ -0,0 +1,6 @@
+# Echo
+
+> 官网 [https://echo.labstack.com/](https://echo.labstack.com/)
+
+> tutorial[https://www.youtube.com/watch?v=_pww3NJuWnk&index=1&list=PLFmONUGpIk0YwlJMZOo21a9Q1juVrk4YY](https://www.youtube.com/watch?v=_pww3NJuWnk&index=1&list=PLFmONUGpIk0YwlJMZOo21a9Q1juVrk4YY)
+> code example [https://github.com/verybluebot/echo-server-tutorial/](https://github.com/verybluebot/echo-server-tutorial/)
